95b00167a14010067871ab833dad3379c3025571
JOGAWILL <CriatividadeZeroZero@hotmail.com>
2025-11-25 22:20:18 -0300

    Delete js directory

--- js/list-posts.js
+++
@@ -1,9 +0,0 @@
-
-function loadPosts(jsonPath){
- fetch(jsonPath).then(r=>r.json()).then(data=>{
-  let c=document.getElementById('lista'); 
-  data.posts.forEach(p=>{
-   c.innerHTML += `<div class="card"><h3>${p.title}</h3><a href="${p.url}">Ler mais</a></div>`;
-  });
- });
-}
